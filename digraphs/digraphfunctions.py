a9888e21ee255357ea597e7b5df431e15284bb5f
cb-g <55954688+cb-g@users.noreply.github.com>
2022-10-02 12:38:55 +0200

    Add files via upload

---
+++ digraphs/digraphfunctions.py
@@ -0,0 +1,14 @@
+# originally until_x() from here: https://stackoverflow.com/questions/68905829/creating-a-newline-in-a-string-after-20-characters-at-the-end-of-a-word-python
+def autolinebreak(string: str, x: int = 20) -> str:
+    
+     lst = string.split()
+     line = ''
+     str_final = ''
+     for word in lst:
+         if len(line + ' ' + word) <= x:
+             str_final += word + ' '
+             line += word + ' '
+         else:
+             str_final += '\n' +  word + ' '
+             line = word + ' '
+     return str_final
